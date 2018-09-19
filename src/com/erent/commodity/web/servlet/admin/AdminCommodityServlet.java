66d3181cf738bd201621e487a2ac38ffdd6125f3
sweey <960102512@qq.com>
2018-09-20 00:31:45 +0800

    恢复adminCommodityServlet.java

---
+++ src/com/erent/commodity/web/servlet/admin/AdminCommodityServlet.java
@@ -0,0 +1,83 @@
+package com.erent.commodity.web.servlet.admin;
+
+
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.beanutils.ConversionException;
+import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.commons.beanutils.Converter;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
+
+import com.erent.commodity.domain.Commodity;
+import com.erent.commodity.service.CommodityException;
+import com.erent.commodity.service.CommodityService;
+import com.erent.utils.BaseServlet;
+import com.erent.utils.JsonDateValueProcessor;
+import com.erent.utils.TransformUtils;
+
+/**
+ * @author acer-pc
+ *
+ */
+
+public class AdminCommodityServlet extends BaseServlet {
+	private CommodityService service = new CommodityService();
+	
+	
+	/**
+	 * 管理员获取商品
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public void getCommodity(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String nowPage = request.getParameter("nowPage");
+		String pageSize = request.getParameter("pageSize");
+
+		List<Commodity> result = null;
+		try {
+			result = service.adminGetCommodity(nowPage, pageSize);
+			response.getWriter().print(TransformUtils.returnArrayString(result));
+		} catch (CommodityException e) {
+			JSONObject object = TransformUtils.packException(e);
+			response.getWriter().print(object.toString());
+		}		
+	}
+	
+	/**
+	 * 管理员删除商品
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public void deleteCommodity(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String commodityId = request.getParameter("commodity_id");
+
+		List<Commodity> result = null;
+		try {
+			service.remove(commodityId);
+			JSONObject object = new JSONObject();
+			object.accumulate("flag", true);
+			response.getWriter().print(object.toString());
+		} catch (CommodityException e) {
+			JSONObject object = TransformUtils.packException(e);
+			response.getWriter().print(object.toString());
+		}		
+	}
+}
